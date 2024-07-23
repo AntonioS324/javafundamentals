dcfe259ddc75bf1c1d33719f37860646adb21af2
Antonio S <stricklandantonio99@yahoo.com>
2024-07-23 18:00:42 -0500

    LoopingLab

--- Java Fundamentals/src/main/java/com/fundamentals/labs/LoopingLab.java
+++ Java Fundamentals/src/main/java/com/fundamentals/labs/LoopingLab.java
@@ -1,2 +1,36 @@
-package com.fundamentals.labs;public class LoopingLab {
+package com.fundamentals.labs;
+
+public class LoopingLab {
+
+    public void Task1() {
+        int count = 0;
+
+        while (count <= 15) {
+            if (count == 5) {
+
+                System.out.println("Five");
+            } else if (count == 10) {
+
+                System.out.println("Ten");
+            } else if (count == 15) {
+                System.out.println("Fifteen");
+            } else {
+
+                System.out.println(count);
+            }
+            count++;
+        }
+    }
+
+
+    public void task2() {
+        for (int i = 0; i <= 30; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+
 }
+
